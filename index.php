892d4ace170c9b9a9c259d93a9b4b73fb97fec2e
SociallyEncrypted404 <SociallyEncrypted.404@gmail.com>
2026-03-26 03:13:50 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>Void Cryptrix Lab</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<h1>Void://Cryptrix Security Lab</h1>
+
+<ul>
+
+<li><a href="login.php">login</a></li>
+
+<li><a href="search.php">search</a></li>
+
+<li><a href="upload.php">upload</a></li>
+
+<li><a href="cmd.php">cmd panel</a></li>
+
+<li><a href="csrf.php">csrf</a></li>
+
+<li><a href="session.php">session</a></li>
+
+<li><a href="reset.php">reset</a></li>
+
+<li><a href="jwt.php">jwt</a></li>
+
+<li><a href="api/user.php?id=1">api</a></li>
+
+<li><a href="fakeadmin.php">dev panel</a></li>
+
+<li><a href="log.php">logs</a></li>
+
+</ul>
+
+</body>
+
+</html>
